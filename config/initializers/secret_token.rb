6ad14538487cdff880bcdca7dd4cd1b6f85d3c46
gablg1 <gabrielg@Gabriels-MacBook-Pro.local>
2014-05-25 15:50:03 -0300

    Brasa init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brasa::Application.config.secret_key_base = 'aa9f214092a90362d3b3c74a29fbf50dfe695fa4589195ab36ec0950360ad9a183a475b868230b43f4f7dc30283d273c16626166941b6107a109587c767f5f0a'
