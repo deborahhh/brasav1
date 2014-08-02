ec2ff79c273b389c3bca3830ab17038a790249c0
gablg1 <gabrielg@Gabriels-MacBook-Pro.local>
2014-08-02 20:02:49 -0300

    User Form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,26 @@
 class User < ActiveRecord::Base
+  # Fields are name, email, graduation_year, major, university, gender, role
+  validates :name, :email, :graduation_year, :major, :university, :role, :presence => true
+
+  before_validation(on: :create) do
+    if self.role.blank?
+      self.set_regular!
+    end
+  end
+  
+  def set_regular!
+    self.role = "regular"
+  end
+  
+  def regular?
+    self.role == "regular"
+  end
+  
+  def set_admin!
+    self.role = "admin"
+  end
+  
+  def admin?
+    self.role == "admin"
+  end
 end
