74041c310bb7cafa9a34d7242e56d7773a3b7296
gablg1 <gabrielg@Gabriels-MacBook-Pro.local>
2014-08-18 15:52:51 -0300

    Universities are constant array

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    # Splits universities into three columns
+    @unis = UNIVERSITIES.in_groups(3, false)
   end
   
   def estudar_nos_eua
---
+++ config/initializers/constants.rb
@@ -0,0 +1,8 @@
+UNIVERSITIES = ["Brown University", "California Institute of Technology", "Columbia University", "Cornell University", 
+              "Georgetown University", 
+              "Georgia Institute of Technology", "Harvard University", "Massachusetts Institute of Technology", 
+              "New York University", "Ohio State University", "Pennsylvania State University", "Rhode Island School of Design", 
+              "Stanford University", 
+              "The University of Chicago", "University of California, Berkeley", "University of California, Davis", 
+              "University of California, San Diego", "University of Michigan", "University of Miami", "University of Pennsylvania", 
+              "University of Southern California", "University of Virginia", "Yale University", "Worcester Polytechnic Institute"]
